a84fc49b0a4573f1ce2fb82cb4ec6b159cabcefb
Severi Pietikäinen <severi.pietikainen1@gmail.com>
2025-03-26 11:29:57 +0200

    Update translations.js

--- Public/js/translations.js
+++ Public/js/translations.js
@@ -1,21 +1,28 @@
 function setLanguage(lang) {
-    
     fetch('translations.json')
         .then(response => response.json())
         .then(data => {
-            const translations = data;           
+            const translations = data;
             document.querySelectorAll("[data-i18n]").forEach(element => {
                 const key = element.getAttribute("data-i18n");
                 if (translations[lang] && translations[lang][key]) {
                     element.textContent = translations[lang][key];
                 }
             });
+
+            // Update the PDF based on the selected language
+            currentPDF = lang === 'fi' ? 'assets/documents/CV_fi.pdf' : 'assets/documents/CV_enf.pdf';
+            document.querySelector("#resumeContent a").href = currentPDF;
+
+            // Reload the PDF immediately after the language change
+            loadPDF(currentPDF); 
         })
+        .catch(error => console.error('Error loading translations:', error));
 }
 
-
+// Set initial language to English
 setLanguage('en');
 
-
+// Event listeners for language change
 document.querySelector('[onclick="setLanguage(\'fi\')"]').addEventListener('click', () => setLanguage('fi'));
 document.querySelector('[onclick="setLanguage(\'en\')"]').addEventListener('click', () => setLanguage('en'));
